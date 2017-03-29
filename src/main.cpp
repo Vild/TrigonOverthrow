702126017c2306dacfcbcabeb06a37b3124c8825
Henrik Vik <henkvik@gmail.com>
2017-03-29 14:51:23 +0200

    vs conficts

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 // This is a personal academic project. Dear PVS-Studio, please check it.
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 #include "engine.hpp"
+#include <SDL2/SDL.h>
 #include <cstdio>
 #include <stdlib.h>
 #include <time.h>
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv) 
+{
 	srand(time(NULL));
 	try {
 		return Engine::getInstance().run();
