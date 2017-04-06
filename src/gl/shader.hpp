a84c59ffa9c2e94b089a355b3abfa0c89c8fa50a
Henrik Vik <henkvik@gmail.com>
2017-04-06 14:50:41 +0200

    ssao render system begin

--- src/gl/shader.hpp
+++ src/gl/shader.hpp
@@ -127,7 +127,7 @@ public:
 		try {
 			_glUniform(_uniform.at(name), 1, &value);
 		} catch (std::out_of_range& e) { //-V565
-																		 // std::cerr << "Uniform is missing! Did you forget to use that variable in that shader?: " << name << std::endl;
+			// std::cerr << "Uniform is missing! Did you forget to use that variable in that shader?: " << name << std::endl;
 			// throw ShaderProgramException(std::string("Uniform is missing! Did you forget to use that variable in that shader?: ") + name);
 		}
 		return *this;
