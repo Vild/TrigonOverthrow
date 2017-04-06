7b097b3e3a070d5394fa8fba86afabb58e57530b
Henrik Vik <henkvik@gmail.com>
2017-04-06 15:27:34 +0200

    big commit

--- src/world/system/SSAORenderSystem.cpp
+++ src/world/system/SSAORenderSystem.cpp
@@ -1,10 +1,11 @@
 #include "SSAORenderSystem.hpp"
+#include <random>
 
 SSAORenderSystem::SSAORenderSystem(int width, int height)
 {
 	shaderProgram
-		.attach("", ShaderType::vertex)
-		.attach("", ShaderType::fragment)
+		.attach("assets/shaders/ssao.vert", ShaderType::vertex)
+		.attach("assets/shaders/ssao.frag", ShaderType::fragment)
 		.finalize();
 
 	shaderProgram
@@ -19,7 +20,10 @@ SSAORenderSystem::SSAORenderSystem(int width, int height)
 		.addUniform("viewMatrix")
 		.addUniform("projectionMatrix");
 
+	gBuffer.attachTexture(0, width, height, GL_RED, GL_FLOAT, 1);
 
+	shaderProgram.bind();
+	generateUniformData();
 }
 
 GBuffer & SSAORenderSystem::render(GBuffer & prevBuffer, Camera & camera)
@@ -32,3 +36,49 @@ GBuffer & SSAORenderSystem::render(GBuffer & prevBuffer, Camera & camera)
 	
 	return gBuffer;
 }
+
+float SSAORenderSystem::lerp(float a, float b, float f)
+{
+	return a + f * (b - a);
+}
+
+void SSAORenderSystem::generateUniformData()
+{
+	std::uniform_real_distribution<GLfloat> randomFlaots(0.0, 1.0);
+	std::default_random_engine generator;
+
+	std::vector<glm::vec3> samplePoints;
+	for (size_t i = 0; i < 64; i++)
+	{
+		glm::vec3 samplePoint = {
+			randomFlaots(generator) * 2.0 - 1.0,
+			randomFlaots(generator) * 2.0 - 1.0,
+			randomFlaots(generator)
+		};
+
+		samplePoint = glm::normalize(samplePoint);
+		samplePoint *= randomFlaots(generator);
+
+		float scale = float(i) / 64.0;
+		scale = lerp(0.1, 1.0, scale * scale);
+
+		samplePoint *= scale;
+
+		samplePoints.push_back(samplePoint);
+	}
+
+	shaderProgram.setUniformArray("samplePoints", samplePoints);
+
+	std::vector<glm::vec3> noiseData;
+	for (size_t i = 0; i < 16; i++)
+	{
+		glm::vec3 noise = {
+			randomFlaots(generator) * 2.0 - 1.0,
+			randomFlaots(generator) * 2.0 - 1.0,
+			0.0
+		};
+
+		noiseData.push_back(noise);
+	}
+}
+
