4e43faaaa5dd3884bdd327cccdd273bb02ea66f3
Henrik Vik <henkvik@gmail.com>
2017-04-06 18:06:44 +0200

    moved velocity and acceleration from transform component

--- src/world/component/transformcomponent.hpp
+++ src/world/component/transformcomponent.hpp
@@ -1,17 +1,15 @@
 #pragma once
-
 #include "component.hpp"
-
 #include <glm/glm.hpp>
-#include <glm/gtc/type_ptr.hpp>
 
-struct TransformComponent : public Component<TransformComponent> {
-	glm::vec3 position = glm::vec3{0, 0, 0};
-	glm::vec3 velocity = glm::vec3{0, 0, 0};
-	glm::vec3 acceleration = glm::vec3{0, 0, 0};
+struct TransformComponent : public Component<TransformComponent> 
+{
+	glm::vec3 position = glm::vec3(0, 0, 0);
+	glm::vec3 scale = glm::vec3(1, 1, 1);
+	glm::vec3 rotation = glm::vec3(0, 0, 0);
+	glm::mat4 matrix = glm::mat4();
 
-	glm::vec3 scale = glm::vec3{1, 1, 1};
-	glm::mat4 rotation = glm::mat4();
+	void updateMatrix();
 
 	virtual void registerImGui();
 
