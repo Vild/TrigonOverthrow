b94d73b1aeef56e1c74399d54d41f4b31226178d
Dan Printzell <xwildn00bx@gmail.com>
2017-04-09 01:58:46 +0200

    Ran PVS-Studio and fixed the problems
    Signed-off-by: Dan Printzell <xwildn00bx@gmail.com>

--- src/world/component/cameracomponent.cpp
+++ src/world/component/cameracomponent.cpp
@@ -1,3 +1,5 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 #include "cameracomponent.hpp"
 #include <glm/gtc/type_ptr.hpp>
 #include <glm/gtc/matrix_transform.hpp>
