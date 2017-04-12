9f364f67e4abcdf134ec8873dfd497461cca2e9e
Dan Printzell <xwildn00bx@gmail.com>
2017-04-12 14:00:54 +0200

    Small changes to CollisionBox2D
    Signed-off-by: Dan Printzell <xwildn00bx@gmail.com>

--- src/utils/collisionbox.hpp
+++ src/utils/collisionbox.hpp
@@ -2,14 +2,18 @@
 
 #include <glm/glm.hpp>
 
-class CollisionBox2D {
-public:
-	inline bool collide(const glm::vec2& point) { return point.x > _pos.x && point.x < _pos.x + _size.x && point.y > _pos.y && point.y < _pos.y + _size.y; }
-	inline bool collide(const CollisionBox2D& other) {
-		return _pos.x < other._pos.x + other._size.x && _pos.x + _size.x > other._pos.x && _pos.y < other._pos.y + other._size.y && _pos.y + _size.y < other._pos.y;
+struct CollisionBox2D {
+	glm::vec2 position;
+	glm::vec2 size;
+
+	CollisionBox2D(glm::vec2 position, glm::vec2 size) : position(position), size(size) {
 	}
 
-private:
-	glm::vec2 _pos;
-	glm::vec2 _size;
+	inline bool collide(const glm::vec2& point) {
+		return point.x > position.x && point.x < position.x + size.x && point.y > position.y && point.y < position.y + size.y;
+	}
+	inline bool collide(const CollisionBox2D& other) {
+		return position.x < other.position.x + other.size.x && position.x + size.x > other.position.x && position.y < other.position.y + other.size.y &&
+					 position.y + size.y < other.position.y;
+	}
 };
