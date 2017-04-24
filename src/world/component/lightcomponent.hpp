62ef9bd23273e6295af7ce3db2f6a0ea9ce54aa2
Dan Printzell <xwildn00bx@gmail.com>
2017-04-24 16:36:43 +0200

    Started to work on LightComponent
    Signed-off-by: Dan Printzell <xwildn00bx@gmail.com>

--- src/world/component/lightcomponent.hpp
+++ src/world/component/lightcomponent.hpp
@@ -2,8 +2,19 @@
 
 #include "component.hpp"
 
+#include <glm/glm.hpp>
+
 struct LightComponent : public Component {
 	virtual ~LightComponent();
 
+	// glm::vec3 pos;
+	float radius;
+	glm::vec3 color;
+	float linear;
+	float quadratic;
+	float yaw;
+	float pitch;
+
+	virtual void registerImGui();
 	virtual std::string name() { return "LightComponent"; }
 };
