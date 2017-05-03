90b4530687469a6265419140223d9e860c3c855b
Henrik Vik <henkvik@gmail.com>
2017-05-03 16:16:37 +0200

    floor rising

--- src/world/system/floortilesystem.cpp
+++ src/world/system/floortilesystem.cpp
@@ -2,8 +2,6 @@
 #include "../component/floortilecomponent.hpp"
 #include "../component/rigidbodycomponent.hpp"
 
-#include <minmax.h>
-
 FloorTileSystem::FloorTileSystem()
 {
 	this->playerTransform = nullptr;
@@ -20,24 +18,52 @@ void FloorTileSystem::setPlayerTransform(TransformComponent * playerTransform)
 
 void FloorTileSystem::update(World & world, float delta)
 {
+	static auto clamp = [](float v, float lo, float hi) -> float
+	{
+		if (v < lo) return lo;
+		if (v > hi) return hi;
+		return v;
+	};
+
+	static auto max = [](float a, float b) -> float
+	{
+		if (a > b) return a;
+		return b;
+	};
+
 	glm::vec3 playerPos = playerTransform->getPosition();
 	for (auto& entity : world.getEntities())
 	{
 		FloorTileComponent* ftc = entity->getComponent<FloorTileComponent>();
-		TransformComponent* tc = entity->getComponent<TransformComponent>();
-		RigidBodyComponent* rbc = entity->getComponent<RigidBodyComponent>();
 
-		if (ftc && tc)
+		if (ftc)
 		{
-			float height = ftc->getHeight();
-			glm::vec3 tilePos = tc->getPosition();
-			float distance = glm::distance(playerPos, tilePos);
+			TransformComponent* tc = entity->getComponent<TransformComponent>();
+			if (tc)
+			{
+				float heightFactor = ftc->getHeightFactor();
 
+				if (heightFactor < 1.0f)
+				{
+					float height = ftc->getHeight();
+					glm::vec3 tilePos = tc->getPosition();
+					float distance = glm::distance(playerPos, tilePos);
 
-			float heightFactor =  1 - min(1, max(0.1, log2(max(0,distance - 3))));
+					if (height > 0 && distance < 10)
+					{
+						heightFactor = max(heightFactor, 1.0f - clamp(log2(max(0, distance - 4.0f)) * 0.5f, 0.0f, 1.0f));
+						
+						ftc->setHeightFactor(heightFactor);
+						tc->setPosition({ tilePos.x, height * heightFactor, tilePos.z });
 
-			tc->setPosition({tilePos.x, height * heightFactor, tilePos.z});
-			rbc->setTransform(tc);
+						RigidBodyComponent* rbc = entity->getComponent<RigidBodyComponent>();
+						if (rbc)
+						{
+							rbc->setTransform(tc);
+						}
+					}
+				}
+			}
 		}
 	}
 }
