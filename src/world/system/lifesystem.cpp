428360c0e5bc3d6187eb766aa44764c15201f32c
Dan Printzell <xwildn00bx@gmail.com>
2017-05-26 11:48:25 +0200

    Added WinState trigger
    Signed-off-by: Dan Printzell <xwildn00bx@gmail.com>

--- src/world/system/lifesystem.cpp
+++ src/world/system/lifesystem.cpp
@@ -14,8 +14,10 @@
 #include "../../io/jsonloader.hpp"
 #include "../../engine.hpp"
 
+#include "../../state/winstate.hpp"
+
 void LifeSystem::update(World& world, float delta) {
-	for (Entity * entity : world.getActiveComponents<LifeComponent>()) {
+	for (Entity* entity : world.getActiveComponents<LifeComponent>()) {
 		auto lifeComp = entity->getComponent<LifeComponent>();
 		if (!lifeComp)
 			continue;
@@ -35,19 +37,20 @@ void LifeSystem::update(World& world, float delta) {
 				rdbComp->setHitboxHalfSize(oldTransComp->getScale());
 				rdbComp->setActivationState(DISABLE_DEACTIVATION);
 				newParticleComp->addEmitter(oldTransComp->getPosition(), glm::vec3(0, 1, 0), ParticleComponent::ParticleEffect::ORB);
-				Engine::getInstance().getSystem<BulletPhysicsSystem>()->addRigidBody(rdbComp,
-					BulletPhysicsSystem::CollisionType::COL_EXP_ORB,
-					BulletPhysicsSystem::orbCollidesWith);
+				Engine::getInstance().getSystem<BulletPhysicsSystem>()->addRigidBody(rdbComp, BulletPhysicsSystem::CollisionType::COL_EXP_ORB,
+																																						 BulletPhysicsSystem::orbCollidesWith);
 
 				Engine::getInstance().getSystem<RoomLoadingSystem>()->enemyDead(world, entity);
 			}
 		}
 
-		if ((entity->getComponent<ProjectileComponent>() || entity->getComponent<ParticleComponent>()) &&
-			!entity->getComponent<AIComponent>() && !entity->getComponent<BossAIComponent>()) {
+		if ((entity->getComponent<ProjectileComponent>() || entity->getComponent<ParticleComponent>()) && !entity->getComponent<AIComponent>() &&
+				!entity->getComponent<BossAIComponent>()) {
 			lifeComp->currHP -= 1 * delta;
 		}
 
+		if (entity->getComponent<BossAIComponent>() && !lifeComp->currHP)
+			Engine::getInstance().setState<WinState>();
 	}
 }
 
